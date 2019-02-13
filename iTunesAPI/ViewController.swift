cfeb097da458fae324550e12d0753f25108235fd
ykharyt <ykharyt@softserveinc.com>
2019-02-13 21:56:17 +0200

    Added XCode project to the repository

---
+++ iTunesAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesAPI
+//
+//  Created by Yevhen Kharytonenko on 2/13/19.
+//  Copyright © 2019 Yevhen Kharytonenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
